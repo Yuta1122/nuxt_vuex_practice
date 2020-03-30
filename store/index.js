ee61df92836d4d5d7fc52c453ac2df582c067459
shimabee <mrs1122ani@gmail.com>
2020-03-30 23:37:41 +0900

    set mutation SET_QUESTION, SET_ANSWER

--- store/index.js
+++ store/index.js
@@ -12,5 +12,13 @@ export const mutations = {
   //counterの値を-1するmutation
   decrement(state){
       state.counter--
+  },
+  // fetchQuestionによって得られたyaml内の質問と選択肢を、stateのquestionsに書き換える処理
+  SET_QUESTION: function(state,question) {
+    state.questions=question;
+  },
+  // fetchAnswerによって得られたおすすめカード情報を、stateのanswerに書き換える処理
+  SET_ANSWER: function(state,answer) {
+    state.answer=answer;
   }
 }
